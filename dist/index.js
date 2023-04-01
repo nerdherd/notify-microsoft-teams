8beb5f6e96eba00bd56933b9b5f1cc1834378947
Kyle Huang <brokenfloppydisk@users.noreply.github.com>
2023-03-31 21:17:46 -0700

    Modify text

--- dist/index.js
+++ dist/index.js
@@ -1427,7 +1427,7 @@ class MSTeams {
 		if (commits !== null) {
 			var payloadText = ''
 			for (var i = 0; i<commits.length; i++) {
-				payloadText += `${commits[i].author.name} <${commits[i].author.email}> commited at (${commits[i].timestamp}): ${commits[i].message}\n\n`
+				payloadText += `${commits[i].author.name} <${commits[i].author.email}> commited at ${commits[i].timestamp}: ${commits[i].message}\n\n`
 			}
 			payload.text = payloadText
 		}
